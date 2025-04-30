0b274db04060dbb7087bb9b299865571c9540e0e
Sebastian Villa <sebasviro567@gmail.com>
2025-04-30 13:31:52 -0500

    Implementar algunos metodos

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -6,29 +6,48 @@ class ReglaValidacion(ABC):
         self._longitud_esperada = _longitud_esperada
 
     def _validar_longitud(self, clave: str) -> bool:
-        pass
+        return  len(clave) > self._longitud_esperada
+
     def _contiene_mayuscula(self, clave: str) -> bool:
-        pass
+        return any(c.isupper() for c in clave)
 
     def _contiene_minuscula(self, clave: str) -> bool:
-        pass
+        return  any(c.islower() for c in clave)
 
     def _contiene_numero(self, clave: str) -> bool:
-        pass
+        return any(c.isdigit() for c in clave)
 
     @abstractmethod
     def es_valida(self, clave: str) -> bool:
         pass
 
-    class ReglaValidacionGanimedes(ReglaValidacion):
-        def _init_(self):
-            pass
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        super().__init__(8)
 
-        def contiene_caracter_especial(self, clave: str) -> bool:
-            pass
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        especiales = "@_#$%"
+        return any(c in especiales for c in clave)
 
-        def es_valida(self, clave: str) -> bool:
-            pass
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    def __init__(self):
+        super().__init__(6)
+
+    def contiene_calisto(self, clave: str) -> bool:
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+class Validador:
+    def __init__(self, regla: ReglaValidacion):
+        self.regla = regla
+
+    def es_valida(self, clave: str) -> bool:
+        return self.regla.es_valida(clave)
 
 
 
